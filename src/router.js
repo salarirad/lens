4f4be6bcee1bd5a3728fd2a08473d28205f25cb8
Morteza Ansarinia <ansarinia@me.com>
2020-03-10 18:39:26 +0100

    add initial structure including router, app, etc

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+//main components
+import Study from './study';
+import About from './about';
+
+export default function AppRouter() {
+  return (
+    <Router basename="/">
+        <Switch>
+          <Route exact path="/"><About /></Route>
+          <Route path="/s"><Study /></Route>
+          <Route path="/about"><About /></Route>
+        </Switch>
+    </Router>
+  );
+}
