3ed9a2fa98d072dbcfe499d80057c8f527939296
Morteza Ansarinia <ansarinia@me.com>
2020-04-04 19:56:24 +0200

    fix BART and add i18n keys

--- src/bart.js
+++ src/bart.js
@@ -17,21 +17,23 @@ export default function BART({content, onStore, onFinish, showStudyNav}) {
   const { t } = useTranslation();
   const {reward, maxPumps, initialPumps, trials} = content;
 
-  const [pumps, setPumps] = useState(0);
-  const [finished, setFinished] = useState(false);
-  const [trial, setTrial] = useState(1);
-  const [totalScore, setTotalScore] = useState(0);
-  const [responses, setResponses] = useState([]);
-  const [dialogIsOpen, setDialogIsOpen] = useState(false);
+  const [state, setState] = useState({
+    pumps: 0,
+    finished: false,
+    trial: 1,
+    totalScore: 0,
+    responses: [],
+    dialogIsOpen: false
+  });
 
   // when finished, store responses and proceed to the next view
   useEffect(() => {
-    if (finished && !dialogIsOpen) {
+    if (state.finished && !state.dialogIsOpen) {
       onFinish();
-      onStore(responses);
+      onStore(state.responses);
       showStudyNav(true);
     }
-  }, [finished, dialogIsOpen]);
+  }, [state]);
 
   /**
    * Store trial responses, then proceed to the next trial or finish the game
@@ -39,20 +41,22 @@ export default function BART({content, onStore, onFinish, showStudyNav}) {
    * @param {*} explosionProbability last probability of balloon getting exploded
    */
   const newTrial = (cashed, explosionProbability) => {
-    setDialogIsOpen(true);
-
-    setResponses(responses.concat([{
-      trial: trial,
-      risk: 100 / (maxPumps - pumps + 1),
-      pumps: pumps,
-      explosionProbability: explosionProbability,
-      score: cashed? pumps * reward : 0,
-      result: cashed? "cashed" : "exploded"
-    }]));
-
-    setFinished(trial >= trials);
-    setPumps(0);  
-    setTrial(trial+1);
+    setState({
+      ...state, 
+      dialogIsOpen: true,
+      responses: [...state.responses, {
+        trial: state.trial,
+        risk: 100 / (maxPumps - state.pumps + 1),
+        pumps: state.pumps,
+        explosionProbability: explosionProbability,
+        score: cashed? state.pumps * reward : 0,
+        result: cashed? "cashed" : "exploded"
+      }],
+      finished: (state.trial>=trials),
+      pumps: 0,
+      trial: state.trial+1,
+      totalScore: state.totalScore + (cashed?state.pumps*reward:0)
+    });
   }
 
   /**
@@ -60,13 +64,16 @@ export default function BART({content, onStore, onFinish, showStudyNav}) {
    */
   const onInflate = () => {
 
-    let risk = 100 / (maxPumps - pumps + 1);
+    let risk = 100 / (maxPumps - state.pumps + 1);
     let prob = Math.ceil(Math.random() * 100);
 
-    if ((prob >= risk) && pumps > initialPumps) {
+    if ((prob >= risk) && state.pumps > initialPumps) {
       newTrial(false, prob);
     } else {
-      setPumps(pumps+1);
+      setState({
+        ...state,
+        pumps: state.pumps+1
+      });
     }
   };
 
@@ -74,39 +81,31 @@ export default function BART({content, onStore, onFinish, showStudyNav}) {
    * action to cash in the reward
    */
   const onCashIn = () => {
-    let score = pumps * reward;
-    setTotalScore(totalScore + score);
     newTrial(true);
   };
   
-  let bubbleStyle = {
-    width: (pumps+1) * 20,
-    height: (pumps+1) * 20,
-    transition: (pumps===0)?'':'width 1s, height 1s' //explosition and pumping effects
-  };
-
   /**
    * render a dialog that shows trial summary.
    */
   const renderDialog = () => {
     return (
     <Dialog
-      open={dialogIsOpen}
-      onClose={() => setDialogIsOpen(false)}
+      open={state.dialogIsOpen}
+      onClose={() => setState({...state, dialogIsOpen: false})}
       disableBackdropClick
       disableEscapeKeyDown
       aria-labelledby="dialog-title"
     >
-        <DialogTitle id="dialog-title"><b>{responses[responses.length - 1].result==='cashed'?t('bart.cashed_title'):t('bart.exploded_title')}</b></DialogTitle>
+        <DialogTitle id="dialog-title"><b>{state.responses[state.responses.length - 1].result==='cashed'?t('bart.cashed_title'):t('bart.exploded_title')}</b></DialogTitle>
         <DialogContent>
           <DialogContentText>
-          {t('bart.trial_score_report', {score: responses[responses.length - 1].score})}
-          {t('bart.total_score_report', {score: responses.map(r => r.score).reduce((a,b) => a+b, 0)})}
+          {t('bart.trial_score_report', {score: state.responses[state.responses.length - 1].score})}
+          {t('bart.total_score_report', {score: state.responses.map(r => r.score).reduce((a,b) => a+b, 0)})}
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setDialogIsOpen(false)} color="primary" autoFocus size='large'>
-            {trial<=trials?t('bart.next_trial'):t('next')}
+          <Button onClick={() => setState({...state, dialogIsOpen: false})} color="primary" autoFocus size='large'>
+            {state.trial<=trials?t('bart.next_trial'):t('next')}
           </Button>
         </DialogActions>
       </Dialog>);
@@ -118,41 +117,45 @@ export default function BART({content, onStore, onFinish, showStudyNav}) {
   //const render = () => {
     return (
       <Fragment>
-        {dialogIsOpen && renderDialog()}
+        {state.dialogIsOpen && renderDialog()}
   
         <Grid container direction='column' spacing={2} justify="space-between" alignItems='stretch'>
   
         <Grid item container direction='row' justify="space-around" alignItems='center'>
   
-          {trial<=trials && 
+          {state.trial<=trials && 
             <Grid item><Grid container direction='column' justify="space-around" alignItems='center'>
-              {t('bart.next_reward')}<Typography variant="h4">{pumps * reward}</Typography>
+              {t('bart.next_reward')}<Typography variant="h4">{state.pumps * reward}</Typography>
             </Grid></Grid>
           }
   
-          {trial<=trials && 
+          {state.trial<=trials && 
             <Grid item><Grid container direction='column' justify="space-around" alignItems='center'>
-                <Typography color='textSecondary' variant='caption'>{t('bart.trial_label',{trial:trial, trials:trials})}</Typography>
+                <Typography color='textSecondary' variant='caption'>{t('bart.trial_label',{trial:state.trial, trials:trials})}</Typography>
             </Grid></Grid>
           }
   
             <Grid item><Grid container direction='column' justify="space-around" alignItems='center'>
-              {t('bart.total_points')}<Typography variant="h4">{totalScore}</Typography>
+              {t('bart.total_points')}<Typography variant="h4">{state.totalScore}</Typography>
             </Grid></Grid>
         </Grid>
   
         <Grid item><Divider /></Grid>
 
-  
-        <Grid item container direction="column" alignContent='center'> 
-          <div className="bubble-container" style={bubbleStyle}>
-            <figure className="bubble"></figure>
-          </div>
-        </Grid>
-
         <Grid item container direction="row" justify="space-around" alignItems='center'>
           <Fab onClick={onInflate} color='primary'>{t('bart.pump')}</Fab>
           <Fab onClick={onCashIn} color='primary'>{t('bart.cash')}</Fab>
+        </Grid>
+
+        
+        <Grid item container direction="column" alignContent='center'> 
+          <div className="bubble-container" style={{
+            width: (state.pumps+1) * 20,
+            height: (state.pumps+1) * 20,
+            transition: (state.pumps===0)?'':'width 1s, height 1s' //explosition and pumping effects
+          }}>
+            <figure className="bubble"></figure>
+          </div>
         </Grid>
   
   
