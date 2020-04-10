c05a4392395701a92bbce4d2ef8927da56d8608c
Morteza Ansarinia <ansarinia@me.com>
2020-04-10 23:35:47 +0200

    dark theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,5 +1,11 @@
 import {createMuiTheme, responsiveFontSizes} from '@material-ui/core';
 
-const theme = responsiveFontSizes(createMuiTheme());
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  }
+});
+
+const theme = responsiveFontSizes(darkTheme);
 
 export default theme;
