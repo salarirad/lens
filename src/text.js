bd3e36559623b19e06d1dc1a061547b18a7d017c
Morteza Ansarinia <ansarinia@me.com>
2020-04-21 11:49:26 +0200

    add autocomplete for coutries

--- src/text.js
+++ src/text.js
@@ -1,12 +1,16 @@
 import React, { useRef, useEffect, useState, Fragment } from 'react';
 import { Typography, Divider, Box, TextField, Grid } from '@material-ui/core';
 
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 import { useTranslation } from 'react-i18next';
 
 import Markdown from 'react-markdown/with-html';
 
+import countries from './utils/countries';
+
 export default function Text({content, onStore}) {
-  //props: title, text, placeholder, help, required, pattern, instruction
+  //props: title, text, placeholder, help, required, pattern, instruction, autoComplete
 
   const { t } = useTranslation();
 
@@ -20,15 +24,63 @@ export default function Text({content, onStore}) {
     return () => {
       onStore({
         'view': content,
-        'response': response.current
+        'response': response.current.code
       })
     };
   },[]);
 
 
-  const handleChange = (e) => {
-    response.current = e.target.value
+  const handleChange = (e, value) => {
+    response.current = value
     setState({...state, value: response.current});
+  }
+
+
+  // ISO 3166-1 alpha-2
+  // No support for IE 11
+  const countryToFlag = (isoCode) => {
+    return typeof String.fromCodePoint !== 'undefined'
+      ? isoCode
+          .toUpperCase()
+          .replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
+      : isoCode;
+  }
+
+  const getLabel = (country) => {
+    return (
+      <React.Fragment>
+        <span>{countryToFlag(country.code)}</span>
+        {country.label}
+      </React.Fragment>
+    );
+  }
+
+  const CountrySelect = () => {
+
+    return (
+      <Autocomplete
+        id="country-select"
+        options={countries}
+        autoHighlight
+        fullWidth
+        onChange={handleChange}
+        value={state.value}
+        getOptionLabel={(option) => option.label}
+        noOptionsText={t('no_options')}
+        renderOption={(option) => getLabel(option)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={t('choose_a_country')}
+            variant="outlined"
+            inputProps={{
+              ...params.inputProps,
+              autoComplete: 'new-password', // disable autocomplete and autofill
+            }}
+          />
+        )}
+      />
+    );
   }
 
 
@@ -37,9 +89,11 @@ export default function Text({content, onStore}) {
       <Grid item>
         <Markdown source={t(content.text)} escapeHtml={false} className='markdown-text' />
       </Grid>
+
       {!(content.instruction || false) &&
         <Grid item>
-          <TextField label={t(content.placeholder)} variant="filled" fullWidth onChange={handleChange} />
+          {content.autoComplete === 'countries' && <CountrySelect />}
+          {!content.autoComplete && <TextField label={t(content.placeholder)} variant="filled" fullWidth onChange={handleChange} />}
         </Grid>
       }
     </Grid>
