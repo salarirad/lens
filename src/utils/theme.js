b2b54a33a5167214084c016a96db19c2f86297e2
Morteza Ansarinia <ansarinia@me.com>
2020-04-24 16:39:23 +0200

    improve colors

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,14 +2,26 @@ import {createMuiTheme, responsiveFontSizes} from '@material-ui/core';
 
 const darkTheme = createMuiTheme({
   palette: {
-    type: 'dark'
+    type: 'dark',
+    primary: {
+      main: '#757ce8'
+    },
+    secondary: {
+      main: '#ff7961'
+    }
   }
 });
 
 const rtlDarkTheme = createMuiTheme({
   direction: 'rtl',
   palette: {
-    type: 'dark'
+    type: 'dark',
+    primary: {
+      main: '#757ce8'
+    },
+    secondary: {
+      main: '#ff7961'
+    }
   }
 })
 
